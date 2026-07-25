2c831a8065d2a564a00460019a83d7489caddd2a
AussieBox <boxofaustralians@gmail.com>
2026-07-25 17:13:49 +1000

    Add basic function to RenovitePylonBlockEntity (still wip, just need to do visuals)

--- src/main/java/net/apostasy/perpetuity/block/entity/RenovitePylonBlockEntity.java
+++ src/main/java/net/apostasy/perpetuity/block/entity/RenovitePylonBlockEntity.java
@@ -1,9 +1,18 @@
 package net.apostasy.perpetuity.block.entity;
 
+import net.apostasy.perpetuity.block.RenovitePylonBlock;
 import net.apostasy.perpetuity.registry.ModBlockEntities;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.block.entity.BlockEntityTicker;
+import net.minecraft.block.enums.DoubleBlockHalf;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Box;
+import net.minecraft.world.World;
 import org.jspecify.annotations.NonNull;
 import software.bernie.geckolib.animatable.GeoBlockEntity;
 import software.bernie.geckolib.animatable.instance.AnimatableInstanceCache;
@@ -15,6 +24,16 @@ public class RenovitePylonBlockEntity extends BlockEntity implements GeoBlockEnt
 
     public RenovitePylonBlockEntity(BlockPos pos, BlockState state) {
         super(ModBlockEntities.RENOVITE_PYLON, pos, state);
+    }
+
+    public static void tick(World world, BlockPos pos, BlockState state, RenovitePylonBlockEntity entity) {
+        if (world.getTime() % 200 != 0 || state.get(RenovitePylonBlock.HALF) != DoubleBlockHalf.LOWER) return;
+
+        world.getEntitiesByClass(PlayerEntity.class, new Box(pos).expand(10), LivingEntity::isAlive).forEach(player -> {
+            player.getInventory().getMainStacks().stream()
+                    .filter(ItemStack::isDamageable)
+                    .forEach(stack -> stack.setDamage(stack.getDamage()-1));
+        });
     }
 
     @Override
